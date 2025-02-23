0e9b6141a3cf30bb93c597f30f521a786279b42f
BunnyeNyash <otachibosibori@gmail.com>
2025-02-23 06:47:29 +0300

    day 1 of 30 days of Python

---
+++ day01_Introduction/helloworld.py
@@ -0,0 +1,68 @@
+# Introduction to Python
+
+# Question 1: Check python version
+# Answer: python --version
+
+
+# Question 2: Math operations
+print(3 + 4) # addition
+print(3 - 4 ) # subtraction
+print(3 * 4) # multiplication
+print(3 % 4) # modulus
+print(3 / 4) # division
+print(3 ** 4) # exponential
+print(3 // 4) # floor division
+
+
+# Question 3: Strings [Your name, Your name, Your country, I am enjoying 30 days of python]
+print('Bosi')
+print('Otachi')
+print('Kenya')
+print('I am enjoying 30 days of python')
+
+
+# Question 4: Checking data types [10, 9.8, 3.14, 4 - 4j, ['Asabeneh', 'Python', 'Finland'], Your name, Your family name, Your country]
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Bosi'))
+print(type('Otachi'))
+print(type('Kenya'))
+
+
+
+
+""" Example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary """
+# Number
+print(10) # integer
+print(20.4) # float
+print(2 + 4j) # complex
+
+# String
+print('I love swimming and reading novels')
+
+# Boolean
+print(True)
+
+# List
+friends = ['Winter', 'Barbara', 'King', 'Jimmy']
+print(friends)
+
+# Tuple
+values = (20, 'Jane', True, 'John', 10.2)
+print(values)
+
+# Set
+my_values = {'Weddy', 50, 'Manny', 60, 60}
+print(my_values)
+
+# Dictionary
+student1 = {
+    'name': 'Jade Keller',
+    'age': 20,
+    'strong_subject': 'Mathematics',
+    'mean_score': 95
+}
+print(student1)
